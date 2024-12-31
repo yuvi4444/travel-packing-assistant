c4209db4831fcc2f70d9b8fdf3b83000ce77926c
Yuvraj Ralh <83678519+yuvi4444@users.noreply.github.com>
2024-12-31 17:50:15 +1100

    Add .env to .gitignore

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 const db = require('./db');
 
 const app = express();
 
+require('dotenv').config();
+const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+
 app.use(express.static('public')); // Serve static files
 app.use(bodyParser.json()); // Parse JSON requests
+
+
+app.get('/map-api', (req, res) => {
+    const url = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places&callback=initMap`;
+    
+    fetch(url)
+        .then(response => response.text())
+        .then(data => res.send(data))
+        .catch(error => res.status(500).send("Error fetching Google Maps API"));
+});
 
 // Generate a packing list based on destination and dates
 function generatePackingList(destination) {
